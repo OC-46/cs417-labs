230e1ac0a42670aa373ba735463296fcbcb4f298
OC-46 <owen.collins@unh.edu>
2026-03-26 18:49:19 +0000

    Lab 15: Implement TaskScheduler

--- lab15/src/scheduler.py
+++ lab15/src/scheduler.py
@@ -17,7 +17,10 @@ class TaskScheduler:
     def __init__(self):
         """Initialize the scheduler."""
         # TODO: Set up your internal data structures
-        pass
+        self.heap = []
+        self._counter = 0
+        self._len = 0
+
 
     def add_task(self, priority, description):
         """Add a task to the scheduler.
@@ -27,7 +30,9 @@ class TaskScheduler:
             description: A string describing the task.
         """
         # TODO: Push onto the heap with a tiebreaker
-        pass
+        heapq.heappush(self.heap, (priority, self._counter, description))
+        self._counter += 1
+        self._len += 1
 
     def next_task(self):
         """Remove and return the highest-priority task's description.
@@ -36,7 +41,11 @@ class TaskScheduler:
             The description string, or None if empty.
         """
         # TODO: Pop from the heap, return the description
-        pass
+        if self.is_empty():
+            return None
+        else:
+            self._len -= 1
+            return heapq.heappop(self.heap)[2]
 
     def peek(self):
         """Return the highest-priority task's description without removing it.
@@ -45,14 +54,20 @@ class TaskScheduler:
             The description string, or None if empty.
         """
         # TODO: Look at h[0] without popping
-        pass
+        if self.is_empty():
+            return None
+        else:
+            return self.heap[0][2]
+        
+
 
     def __len__(self):
         """Return the number of pending tasks."""
         # TODO: Return the length of the heap
-        pass
+        return self._len
 
     def is_empty(self):
         """Return True if there are no pending tasks."""
         # TODO
-        pass
+        return self._len == 0
+    
